b71b283291ee3f1cbd82338218177f8c2ab3372e
Rafael Pivatto <rafael.pivatto@gmail.com>
2017-05-27 20:29:53 -0300

    Disable defaults commands

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Commando = require('discord.js-commando');
-const bot = new Commando.Client();
+const bot = new Commando.Client({
+    unknownCommandResponse: false
+});
 const args = process.argv;
 
 bot.registry.registerGroup('status', 'Status');
-bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands')/
 
 bot.login(process.env.BOTKEY || args[3]);
