f04c7d1ba25351457e2678cfdfa03289506b1055
Rafael Pivatto <rafael.pivatto@gmail.com>
2017-08-03 23:21:04 -0300

    Improvements

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ hourlyJob.execute();
 
 logger.info('Bot started');
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Mock listening at http://%s:%s', host, port);
