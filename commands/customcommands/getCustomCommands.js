d84e3ddb2645545f7425ac21505b0e26f969164d
Rafael Pivatto <rafael.pivatto@gmail.com>
2017-09-02 22:48:32 -0300

    Adding get custom commands

--- commands/customcommands/getCustomCommands.js
+++ commands/customcommands/getCustomCommands.js
@@ -11,10 +11,10 @@ const wingColor = '#f00000';
 module.exports = class GetCustomCommand extends Command {
     constructor(client) {
         super(client, {
-            name: '@general',
+            name: 'getcustom',
             group: 'customcommands',
             memberName: 'getcustomcommand',
-            description: 'Command to get a custom commands',
+            description: 'Command to get a custom command',
             guildOnly: true,
             patterns: [new RegExp('[a-zA-Z]')]
         });
@@ -22,35 +22,36 @@ module.exports = class GetCustomCommand extends Command {
 
     async run(msg, args) {
         
-        const commandName = String(msg.message.content).replace(args, '').replace('!', '').toLowerCase();
-        logger.info(logName + ' Execute command = ' + commandName + ' by user = ' + msg.message.author.username);
+        if (msg.message.channel.name !== process.env.CUSTOM_COMMANDS_CHANNEL) {
+            return errorMessage.sendSpecificClientErrorMessage(msg, 'Esse comando não pode ser executado nessa sala.');
+        }
         
-        const query = {_id: commandName};
+        logger.info(logName + ' Execute add command by user = ' + msg.message.author.username);
+             
+        if (!args || args.length === 0) {
+            logger.warn(logName + ' Command without args');
+            return errorMessage.sendSpecificClientErrorMessage(msg, 'Execute passando o nome do comando a ser pesquisado, exemplo: !getcustom <nome_comando>');
+        }
+        
+        const query = {_id: args};
         mongoConnection.find(logName, query, 'customCommands', function(error, data) {
             
             if (error) {
-                logger.error(logName + ' Error on find command = ' + commandName, {'error': error});
+                logger.error(logName + ' Error on find command = ' + args, {'error': error});
                 return errorMessage.sendClientErrorMessage(msg);
             }
             if (!data || data.length == 0) {
-                logger.info(logName + ' Commands not found = ' + commandName, {'error': error});
+                logger.info(logName + ' Commands not found = ' + args, {'error': error});
                 return errorMessage.sendSpecificClientErrorMessage(msg, 'Comando não encontrado... :thinking:');
             }
-            const item = data[0];
-            let embed = new RichEmbed()
-                .setColor(wingColor)
-                .setTimestamp()
-                .setFooter('Fly safe cmdr!')
-                .setTitle(getValue(item.title))
-                .setImage(getValue(item.image))
-                .setDescription(getValue(item.content));
+            
+            delete data[0].createDate;
+            delete data[0].createBy;
 
-            return msg.channel.send(item.alert, {'embed': embed});
-        });
-
-        function getValue(param) {
-            return param ? param : '';
-        }
-        
+            const desc = JSON.stringify(data[0], null, '\t');
+            
+            msg.channel.send('Conteúdo do comando: ' + data[0]._id + '\n\n');
+            return msg.channel.send(desc, {code: 'json'});
+        });        
     }
 }    
---
+++ commands/customcommands/runCustomCommands.js
@@ -0,0 +1,56 @@
+const { Command } = require('discord.js-commando');
+const logger = require('heroku-logger');
+const { RichEmbed } = require('discord.js');
+const mongoConnection = require('../../modules/mongoConnection.js');
+const errorMessage = require('../../modules/errorMessage.js');
+
+const logName = '[RunCustomCommand]';
+const doubleWrapLine = '\n\n';
+const wingColor = '#f00000';
+
+module.exports = class GetCustomCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: '@general',
+            group: 'customcommands',
+            memberName: 'runcustomcommand',
+            description: 'Command to run a custom commands',
+            guildOnly: true,
+            patterns: [new RegExp('[a-zA-Z]')]
+        });
+    }
+
+    async run(msg, args) {
+        
+        const commandName = String(msg.message.content).replace(args, '').replace('!', '').toLowerCase();
+        logger.info(logName + ' Execute command = ' + commandName + ' by user = ' + msg.message.author.username);
+        
+        const query = {_id: commandName};
+        mongoConnection.find(logName, query, 'customCommands', function(error, data) {
+            
+            if (error) {
+                logger.error(logName + ' Error on find command = ' + commandName, {'error': error});
+                return errorMessage.sendClientErrorMessage(msg);
+            }
+            if (!data || data.length == 0) {
+                logger.info(logName + ' Commands not found = ' + commandName, {'error': error});
+                return errorMessage.sendSpecificClientErrorMessage(msg, 'Comando não encontrado... :thinking:');
+            }
+            const item = data[0];
+            let embed = new RichEmbed()
+                .setColor(wingColor)
+                .setTimestamp()
+                .setFooter('Fly safe cmdr!')
+                .setTitle(getValue(item.title))
+                .setImage(getValue(item.image))
+                .setDescription(getValue(item.content));
+
+            return msg.channel.send(item.alert, {'embed': embed});
+        });
+
+        function getValue(param) {
+            return param ? param : '';
+        }
+        
+    }
+}    
