1718ed92bddc14edae70b860f8cba4c7ae7fcc66
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-26 18:24:19 -0200

    Firsts steps and login with discord

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 require('discord.js');
 const Commando = require('discord.js-commando');
 const logger = require('heroku-logger');
+const path = require('path');
+global.appRoot = path.resolve(__dirname);
 
 const registryCustomCommands = require('./modules/registry/registryCustomCommands.js');
 const registryJobsExecution = require('./modules/registry/registryJobsExecution.js');
@@ -9,7 +11,7 @@ const registryEndPoints = require('./modules/registry/registryEndPoints.js');
 const registryDiscordEvents = require('./modules/registry/registryDiscordEvents.js');
 const registryControllerToImageContest = require('./modules/registry/registryControllerToImageContest.js');
 
-const logName = '[Index]';
+const logName = '[App]';
 
 const client = new Commando.Client({
     unknownCommandResponse: false,
@@ -23,11 +25,11 @@ client.registry.registerGroup('help');
 client.registry.registerGroup('register');
 client.registry.registerGroup('status');
 client.registry.registerGroup('system');
-client.registry.registerCommandsIn(__dirname + '/commands');
+client.registry.registerCommandsIn(`${global.appRoot}/commands`);
 
 client.login(process.env.BOT_KEY);
 
-client.on('ready', (arg) => {
+client.on('ready', () => {
     client.user.setPresence({ game: { name: '!ajuda', type: 0 } });
     
     registryCustomCommands.execute(client);
--- modules/registry/registryEndPoints.js
+++ modules/registry/registryEndPoints.js
@@ -1,5 +1,6 @@
 const logger = require('heroku-logger');
 const express = require('express');
+const request = require('request');
 
 const fileManagement = require('../service/fileManagement.js');
 
@@ -15,12 +16,53 @@ exports.execute = function() {
         logger.info('Server listening at ' + host + ':' + port);
     });
     
+    app.use(express.static('src/assets'));
+
     app.all('/images/*', function (req, res) {
         fileManagement.loadFile(logName, res, req.path);
     });
-    
+
     app.all('/', function (req, res) {
-        res.status(200).send('Ok!');
+        res.sendFile(`${global.appRoot}/src/site/index/index.html`);
+    });
+
+    app.all('/login', function (req, res) {
+        const clientId = process.env.CLIENT_ID;
+        const urlCallback = process.env.LOGGED_IN_URL_CALLBACK;
+        const scope = process.env.DISCORD_OAUTH_SCOPE;
+        const discordUrl = `https://discordapp.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${urlCallback}&response_type=code&scope=${scope}`;
+        res.redirect(discordUrl);
+    });
+
+    app.all('/loggedIn', function (req, res) {
+        const options = {
+            url: process.env.DISCORD_TOKEN_URL,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                'client_id': process.env.CLIENT_ID,
+                'client_secret': process.env.CLIENT_SECRET,
+                'grant_type': 'authorization_code',
+                'code': req.query.code,
+                'redirect_uri': process.env.LOGGED_IN_URL_CALLBACK,
+                'scope': process.env.DISCORD_OAUTH_SCOPE
+            }
+        };
+
+        request.post(options, (error, response, body) => {
+            const bodyParsed = JSON.parse(body);
+            const options = {
+                url: `${process.env.DISCORD_API_URL}/users/@me`,
+                headers: {
+                    'Authorization': `Bearer ${bodyParsed.access_token}`
+                }
+            };
+            request.get(options, (error, response, body) => {
+                const bodyParsed = JSON.parse(body);
+                res.send(`Bem-vindo ${bodyParsed.username}`);
+            });            
+        });
     });
 };
 
